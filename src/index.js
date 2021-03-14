6a9e3a340ef0221d7a138c1413786c8f673ee4ca
GreyWay2 <greenpeesoleg@gmail.com>
2021-03-14 17:02:09 +0400

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0) {
+        n = -n;
+    }
+    var num = String(n);
+    var r = "";
+    for(var i=num.length-1;i>=0;i--) {
+      r += Number(num[i])
+    }
+    return Number(r);
 }
